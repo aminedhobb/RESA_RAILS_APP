ff590c69ff8fc4e96bfcd9d8b6d5609728314352
Reda HALIB <redahalib@me.com>
2018-04-17 15:05:21 +0100

    controller done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,46 @@
 class ReviewsController < ApplicationController
+  before_action :set_hotel, only: [:show, :edit, :update, :destroy]
   def index
+    @reviews = Review.all
+  end
+
+  def show
   end
 
   def new
-    @hotel = Hotel.find(params[:hotel_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.hotel = Hotel.find(params[:hotel_id])
-    @review.save
+    @review.hotel = @hotel
+    if @review.save
+      redirect_to hotel_path(@hotel)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def update
-
+    @review.update(review_params)
+    redirect_to hotel_path(@hotel)
   end
 
   def destroy
-
+    @review.destroy
+    redirect_to hotel_path
   end
 
   private
 
+  def set_hotel
+    @hotel = Hotel.find(params[:hotel_id])
+  end
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
