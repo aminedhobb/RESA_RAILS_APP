d59549c1dd628c600206f2c50265da56b3bb5732
Amine Bensaid <aminebac13@gmail.com>
2018-04-17 17:17:04 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,5 @@ ActiveRecord::Schema.define(version: 20180417132923) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "rooms"
-  add_foreign_key "bookings", "users"
   add_foreign_key "hotels", "cities"
 end
