9ea4f9eda8775999ba3784b9038706f91b409ddd
zaynab bou <zaynabourdellah@hotmail.com>
2018-04-20 14:47:17 +0100

    lost in tranlsation

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,10 +1,4 @@
 class Hotel < ApplicationRecord
-  include PgSearch
-  pg_search_scope :search_by_name_and_address,
-    against: [ :name, :address ],
-    using: {
-      tsearch: { prefix: true }
-    }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   validates :name, presence: true, uniqueness: true
